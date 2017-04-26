42f088e960a3d667841a68cc36806b42230eba52
Mr. Theodoropoulos <tony.theodoropoulos@ed.amdsb.ca>
2017-04-26 15:12:35 -0400

    First Commit and Publish

---
+++ WorkingTogether/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkingTogether
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "Off")
+            {
+                label1.Text = "On";
+            }
+            else
+            {
+                label1.Text = "Off";
+            }
+        }
+    }
+}
